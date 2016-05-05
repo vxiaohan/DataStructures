982594584e6aada78f9c48ad0d96ae5d5d2084cb
vxiaohan <vxiaohan@outlook.com>
2016-05-05 23:43:32 +0800

    完成二叉查找树的添加删除与查找判断

--- src/com/structures/chapter4/tree/BinarySearchTree.java
+++ src/com/structures/chapter4/tree/BinarySearchTree.java
@@ -1,5 +1,6 @@
 package com.structures.chapter4.tree;
 
+
 public class BinarySearchTree<T extends Comparable<? super T>>  {
 	private BinaryNode<T> rootNode;
 	
@@ -13,10 +14,161 @@ public class BinarySearchTree<T extends Comparable<? super T>>  {
 		return rootNode==null;
 	}
 	public void insert(T content){
+		if (rootNode == null) {
+			rootNode = insert(content, rootNode, null);
+		}
+		else{
+			insert(content,rootNode, null);
+		}
+	}
+	private BinaryNode<T> insert(T content, BinaryNode<T> binaryNode, BinaryNode<T> fatherNode){
+		//递归终止条件
+		if (binaryNode == null) {
+			binaryNode = new BinaryNode<T>(content);
+			if (fatherNode != null) {
+				if (content.compareTo(fatherNode.content)<0) {
+					fatherNode.leftNode = binaryNode;
+				}else{
+					fatherNode.rightNode = binaryNode;
+				}
+			}
+			return binaryNode;
+		}
+		//进行递归
+		int compareResult = content.compareTo(binaryNode.content);
 		
+		if (compareResult < 0) {
+			return insert(content, binaryNode.leftNode, binaryNode);
+		} else if(compareResult > 0){
+			return insert(content, binaryNode.rightNode, binaryNode);
+		}else {
+			//比较结果为0的话也会终止
+		}
+		return binaryNode;
+	}
+	public boolean contains(T content){
+		return contains(content, rootNode);
+	}
+	private boolean contains(T content, BinaryNode<T> binaryNode){
+		if (binaryNode == null) {
+			return false;
+		}
+		int compareResult = content.compareTo(binaryNode.content);
+		if (compareResult < 0) {
+			return contains(content, binaryNode.leftNode);
+		} else if (compareResult > 0) {
+			return contains(content, binaryNode.rightNode);
+		}else{
+			return true;
+		}
+	}
+	public T findMax(){
+		return findMax(rootNode);
+	}
+	private T findMax(BinaryNode<T> startNode){
+		BinaryNode<T> node = startNode;
+		while(node!=null){
+			if (node.rightNode==null) {
+				return node.content;
+			}else{
+				node = node.rightNode;
+			}
+		}
+		return null;
+	}
+	public T findMin(){
+		return findMin(rootNode);
+	}
+	private T findMin(BinaryNode<T> startNode){
+		BinaryNode<T> node = startNode;
+		while(node!=null){
+			if (node.leftNode==null) {
+				return node.content;
+			}else{
+				node = node.leftNode;
+			}
+		}
+		return null;
+	}
+	public void remove(T content){
+		remove(content, rootNode, null);
+	}
+	private BinaryNode<T> remove(T content, BinaryNode<T> node, BinaryNode<T> fatherNode){
+		if (node == null) {
+			return node;
+		}
+		int compareResult = content.compareTo(node.content);
+		if (compareResult < 0) {
+			remove(content,node.leftNode, node);
+		}else if (compareResult > 0) {
+			remove(content, node.rightNode, node);
+		}else {
+			if (node.leftNode == null && node.rightNode == null) {
+				if (node != rootNode) {
+					if (fatherNode.leftNode != null && fatherNode.leftNode == node) {
+						fatherNode.leftNode = null;
+					}else if (fatherNode.rightNode != null && fatherNode.rightNode == node) {
+						fatherNode.rightNode = null;
+					}
+				}else{
+					rootNode = null;
+				}
+			}else if (node.leftNode == null) {
+				if (node != rootNode) {
+					if (fatherNode.leftNode != null && fatherNode.leftNode == node) {
+						fatherNode.leftNode = node.rightNode;
+					}else if (fatherNode.rightNode != null && fatherNode.rightNode == node) {
+						fatherNode.rightNode = node.rightNode;
+					}
+				}else {
+					rootNode = node.rightNode;
+				}
+			}else if(node.rightNode == null){
+				if (node != rootNode) {
+					if (fatherNode.leftNode != null && fatherNode.leftNode == node) {
+						fatherNode.leftNode = node.leftNode;
+					}else if (fatherNode.rightNode != null && fatherNode.rightNode == node) {
+						fatherNode.rightNode = node.leftNode;
+					}
+				}else {
+					rootNode = node.leftNode;
+				}
+			}else {
+				BinaryNode<T> minNodeAtRight = node.rightNode;
+				BinaryNode<T> minNodeFather = node;
+				while(minNodeAtRight!=null){
+					if (minNodeAtRight.leftNode==null) {
+						break;
+					}else{
+						minNodeFather = minNodeAtRight;
+						minNodeAtRight = minNodeAtRight.leftNode;
+					}
+				}
+				if (minNodeAtRight == minNodeFather.leftNode) {
+					minNodeFather.leftNode = minNodeAtRight.rightNode;
+					minNodeAtRight.leftNode = node.leftNode;
+					minNodeAtRight.rightNode = node.rightNode;
+				}
+				if (minNodeAtRight == minNodeFather.rightNode) {
+					minNodeAtRight.leftNode = node.leftNode;
+				}
+				if (node == rootNode) {
+					rootNode = minNodeAtRight;
+				}else{
+					if (node == fatherNode.leftNode) {
+						fatherNode.leftNode = minNodeAtRight;
+					}
+					if (node == fatherNode.rightNode) {
+						fatherNode.rightNode =minNodeAtRight;
+					}
+				}
+				
+			}
+		}
+		return node;
 	}
 	private static class BinaryNode<T>{
-		public BinaryNode<T> fatherBinaryNode;
+		//public BinaryNode<T> fatherBinaryNode;
 		public BinaryNode<T> leftNode;
 		public BinaryNode<T> rightNode;
 		public T content;
--- test/test/structures/chapter4/tree/TestBinarySearchTree.java
+++ test/test/structures/chapter4/tree/TestBinarySearchTree.java
@@ -33,7 +33,64 @@ public class TestBinarySearchTree {
 
 	@Test
 	public void testInsert() {
-		fail("Not yet implemented");
+		assertEquals(true, binarySearchTree.isEmpty());
+		binarySearchTree.insert("1");
+		assertEquals(false, binarySearchTree.isEmpty());
 	}
-
+	@Test
+	public void testContains(){
+		assertEquals(false, binarySearchTree.contains("1"));
+		binarySearchTree.insert("50");
+		binarySearchTree.insert("2");
+		assertEquals(true, binarySearchTree.contains("50"));
+		assertEquals(true, binarySearchTree.contains("2"));
+		assertEquals(false, binarySearchTree.contains("3"));
+		for (int i = 0; i < 100; i++) {
+			binarySearchTree.insert(Integer.toString(i));
+		}
+		for (int i = 0; i < 100; i++) {
+			assertEquals(true, binarySearchTree.contains(Integer.toString(i)));
+		}
+	}
+	
+	@Test
+	public void testFindMax(){
+		binarySearchTree.insert("50");
+		for (int i = 0; i < 100; i++) {
+			binarySearchTree.insert(Integer.toString(i));
+		}
+		assertEquals("99", binarySearchTree.findMax());
+	}
+	@Test
+	public void testFindMin(){
+		binarySearchTree.insert("50");
+		for (int i = 0; i < 100; i++) {
+			binarySearchTree.insert(Integer.toString(i));
+		}
+		assertEquals("0", binarySearchTree.findMin());
+	}
+	@Test
+	public void testRemove(){
+		binarySearchTree.insert("50");
+		assertEquals(true, binarySearchTree.contains("50"));
+		for (int j = 0; j < 100; j++) {
+			for (int i = 0; i < 100; i++) {
+				int k = 100 - i;
+				binarySearchTree.insert(Integer.toString(i));
+				binarySearchTree.insert(Integer.toString(k));
+			}
+			for (int i = 0; i < 100; i++) {
+				assertEquals(true, binarySearchTree.contains(Integer.toString(i)));
+			}
+			
+			binarySearchTree.remove(Integer.toString(j));
+			for (int i = 0; i < 100; i++) {
+				if (i != j) {
+					assertEquals(true, binarySearchTree.contains(Integer.toString(i)));
+				} else {
+					assertEquals(false, binarySearchTree.contains(Integer.toString(i)));
+				}
+			}
+		}
+	}
 }
